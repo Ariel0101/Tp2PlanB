09b81ed3b1f1a8373ddd65ae22d187698173d19d
sebalogue <seba21log@gmail.com>
2018-06-18 14:12:49 -0300

    update cementerio

--- src/Cementerio.java
+++ src/Cementerio.java
@@ -2,17 +2,23 @@ import java.util.ArrayList;
 
 public class Cementerio {
 
-    private ArrayList<Object> cartas;
+    private ArrayList<Carta> cartas;
 
     public Cementerio(){
-        this.cartas = new ArrayList<Object>();
+
+        this.cartas = new ArrayList<>();
+
     }
 
-    public boolean esta(Object unaCarta) {
+    boolean esta(Carta unaCarta) {
+
         return this.cartas.contains(unaCarta);
+
     }
 
-    public void enviar(Object unaCarta) {
+    void enviar(Carta unaCarta) {
+
         this.cartas.add(unaCarta);
+
     }
 }
