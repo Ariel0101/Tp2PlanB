bf251d448a76aa02b5cb1c96373149399a60a584
dima1997 <franki.dimaria@gmail.com>
2018-06-23 17:16:59 -0300

    Subo primera tanda del constructor de cartas. aitsu, agujaAsesina, agresorOscuro, mokeyMokey, alasDellamaPerversa, goblinFalso,Exodia (5 partes), insectoComeHombres, Jinzo7

---
+++ src/ConstructorDeCartas.java
@@ -0,0 +1,68 @@
+public class ConstructorDeCartas {
+    public CartaMonstruo aitsu(){
+        Monstruo aitsu = new Monstruo(100,100);
+        return new CartaMonstruo(aitsu, 5);
+    }
+
+    public CartaMonstruo mokeyMokey(){
+        Monstruo mokeyMokey = new Monstruo(300,100);
+        return new CartaMonstruo(mokeyMokey,3);
+    }
+
+    public CartaMonstruo goblinFalso(){
+        Monstruo goblinFalso = new Monstruo(400,400);
+        return new CartaMonstruo(goblinFalso, 1);
+    }
+
+    public CartaMonstruo agujaAsesina(){
+        Monstruo agujaAsesina = new Monstruo(1200,1000);
+        return new CartaMonstruo(agujaAsesina,4);
+    }
+
+    public CartaMonstruo agresorOscuro(){
+        Monstruo agresorOscuro = new Monstruo(1200,1200);
+        return new CartaMonstruo(agresorOscuro,4);
+    }
+
+    public CartaMonstruo alasDeLlamaPerversa(){
+        Monstruo alasDeLlamaPerversa = new Monstruo(700, 600);
+        return new CartaMonstruo(alasDeLlamaPerversa, 2);
+    }
+
+    public CartaMonstruoJinzo7 jinzo7(){
+        Monstruo jinzo7 = new Monstruo(500,400);
+        return new CartaMonstruoJinzo7(jinzo7,2);
+    }
+
+    public CartaMonstruoComeHombres insectoComeHombres(Campo campoEnemigo){
+        Monstruo insectoComeHombres = new Monstruo(450, 600);
+        EfectoDestruirMonstruo efectoDestruirMonstruo = new EfectoDestruirMonstruo(campoEnemigo);
+        return new CartaMonstruoComeHombres(insectoComeHombres,2, efectoDestruirMonstruo);
+    }
+
+    public CartaMonstruoExodia cabezaDeExodia(){
+        Monstruo cabezaExodia = new Monstruo(1000,1000);
+        return  new CartaMonstruoExodia(cabezaExodia,3);
+    }
+
+    public  CartaMonstruoExodia brazoDerechoExodia(){
+        Monstruo brazoDerechoExodia = new Monstruo(200,300);
+        return new CartaMonstruoExodia(brazoDerechoExodia,3);
+    }
+
+    public  CartaMonstruoExodia brazoIzquierdoExodia(){
+        Monstruo brazoIzquierdoExodia = new Monstruo(200,300);
+        return new CartaMonstruoExodia(brazoIzquierdoExodia,3);
+    }
+
+    public  CartaMonstruoExodia piernaDerechaExodia(){
+        Monstruo piernaDerechaExodia = new Monstruo(200,300);
+        return new CartaMonstruoExodia(piernaDerechaExodia,3);
+    }
+
+    public  CartaMonstruoExodia piernaizquierdaExodia(){
+        Monstruo piernaIzquierdaExodia = new Monstruo(200,300);
+        return new CartaMonstruoExodia(piernaIzquierdaExodia,3);
+    }
+
+}
