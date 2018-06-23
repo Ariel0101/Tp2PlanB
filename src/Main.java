3e73bac442b5b27e43227b19bd41d7eb244ec0fc
dima1997 <franki.dimaria@gmail.com>
2018-06-23 12:25:02 -0300

    Creo Main basico con metodo main, y metodo start para la interfaz visual.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage ventana) throws Exception {
+
+        ventana.setTitle("Al-Go-Oh");
+
+        ventana.show();
+
+    }
+}
