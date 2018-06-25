4263ac29ac076a4f7b36dd15d1f18d0feb01e81c
sebalogue <seba21log@gmail.com>
2018-06-24 23:02:54 -0300

    agrego carpeta imagenes

--- src/CartaMonstruo.java
+++ src/CartaMonstruo.java
@@ -131,7 +131,7 @@ class CartaMonstruo implements Carta {
     @Override
     public Image imagen() {
 
-        return new Image(this.nombre + ".jpg");
+        return new Image("imagenes/" + this.nombre + ".jpg");
     }
 
 }
